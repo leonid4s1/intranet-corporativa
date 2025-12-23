7de40f6a9f4d2f30cb8567419c7a4c9d3ae2ca39
leonid4s1 <leonardotorres319@aragon.unam.mx>
2025-12-23 12:02:19 -0600

    fix(api): corrige import ESM de auth en ruta docs

--- server/src/routes/docs.js
+++ server/src/routes/docs.js
@@ -1,6 +1,6 @@
 // server/src/routes/docs.js (ESM)
 import { Router } from 'express'
-import authenticate from '../middleware/auth'
+import authenticate from '../middleware/auth.js'
 import { listDocs } from '../controllers/docsController'
 
 const router = Router()
